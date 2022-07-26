84cc6cc160e424487ad4428b94984dd2a14a4763
jay28son <100823027+jay28son@users.noreply.github.com>
2022-07-25 19:58:49 -0700

    Add files via upload

--- SQL Friends Project/FriendsInfo.session.sql
+++ SQL Friends Project/FriendsInfo.session.sql
@@ -1,10 +1,2 @@
-select a.id, a.first_name, a.last_name, c.boba_drink
-FROM friendsinfo AS A
-inner JOIN friends_boba_order AS B
-    on A.ID = B.FRIEND_ID
-inner JOIN (SELECT BOBA_drink,boba_id 
-            FROM BOBA_ORDERS
-            GROUP BY boba_id ) as C
-    on b.boba_id = c.boba_id
-order by a.id;
+select * from food
 
